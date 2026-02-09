d332307584ccd2a0d13b22d14ee922465ad4e40c
aykut55 <aykut55@hotmail.com>
2026-02-09 10:49:35 +0300

    - Kullanılacak kütüphaneler projelere eklendi

--- AlgoTrade.Console/Program.cs
+++ AlgoTrade.Console/Program.cs
@@ -3,12 +3,17 @@ using AlgoTrade.Core.Trading;
 
 AppSettings.EnsureDirectories();
 
+Console.Clear();
+Console.WriteLine("#######################################\n");
+
 var trader = new AlgoTrader("MyStrategy");
+
 trader.MessageReceived += message => Console.WriteLine(message);
 
 trader.Start();
-
-Console.WriteLine("Çıkmak için bir tuşa basın...");
-Console.ReadKey();
-
+Thread.Sleep(100);
 trader.Stop();
+
+Console.WriteLine("\n#######################################\n");
+Console.WriteLine("\nÇıkmak için bir tuşa basın...");
+Console.ReadKey();
